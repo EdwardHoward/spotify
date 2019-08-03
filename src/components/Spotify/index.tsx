ffac6bef78a798adb5418a3228e16453720db53b
EdwardHoward <edwardhoward11@gmail.com>
2019-08-02 22:24:13 -0600

    quick

--- src/components/Spotify/index.tsx
+++ src/components/Spotify/index.tsx
@@ -2,22 +2,31 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Timer from '../Timer';
+import { Segment, Container, Header, Icon, Button } from 'semantic-ui-react';
+import * as ReactCountdownClock from 'react-countdown-clock';
 
 export interface ISpotifyProps {
 }
+
+const countdownTime = 10;
 
 export function Spotify(props: ISpotifyProps) {
    const [authorized, setAuthorized] = useState(false);
    const [authenticateLink, setAuthenticateLink] = useState('');
    const [playing, setPlaying] = useState(true);
-   const [currentTime, setCurrentTime] = useState(60);
+   const [currentTime, setCurrentTime] = useState(countdownTime);
+   const [showControl, setShowControl] = useState<boolean>(false);
 
    const url = 'http://192.168.1.177:3030';
    useEffect(() => {
       axios.get(`${url}/login`, { withCredentials: true }).then((data) => {
          console.log(data);
          if (data.data.authenticated) {
-            setPlaying(data.data.playing.body.is_playing);
+            if(data.data.playing.body && data.data.playing.body.is_playing)
+               setPlaying(true);
+            else{
+               setPlaying(false);
+            }
             setAuthorized(true);
          } else {
             setAuthenticateLink(data.data);
@@ -35,13 +44,23 @@ export function Spotify(props: ISpotifyProps) {
       });
    }, []);
 
+    useEffect(() => {
+      if(currentTime == 0){
+         next()
+      }
+
+      if(currentTime == -1){
+         setCurrentTime(countdownTime);
+      }
+    }, [currentTime]);
+
    function play() {
       axios.get(`${url}/play`, { withCredentials: true }).then((data) => {
          setPlaying(true);
       });
    }
 
-   function pause(){
+   function pause() {
       axios.get(`${url}/pause`, { withCredentials: true }).then((data) => {
          setPlaying(false);
       })
@@ -56,36 +75,67 @@ export function Spotify(props: ISpotifyProps) {
    }
 
    function Player(props) {
+      // return (
+      //    <div>
+      //       <button onClick={prev}>Prev</button>
+      //       {props.playing ? <button onClick={pause}>Pause</button> : <button onClick={play}>Play</button>}
+      //       <button onClick={next}>Next</button>
+      //    </div>
+      // )
+
       return (
-         <div>
-            <button onClick={prev}>Prev</button>
-            {props.playing ? <button onClick={pause}>Pause</button> : <button onClick={play}>Play</button>}
-            <button onClick={next}>Next</button>
+         <div className={"hover-opacity" + (props.playing ? ' hidden' : '')}>
+            {(props.playing ?
+               <Playing /> :
+               <Paused />)}
          </div>
       )
    }
 
-   function Authorize(props){
+   function Authorize(props) {
       return (
          <a href={props.link}>Authorize</a>
       )
    }
 
-   function onTick(){
+   async function onTick() {
       setCurrentTime(currentTime - 1);
    }
 
-   function onFinished(){
-      next().then((data) => {
-         console.log(data);
-         setCurrentTime(60);
-      });
+   function onFinished() {
+      
+   }
+
+   function Playing(props) {
+      return (
+         <>
+            <Icon name='pause circle' size="massive" className="icon-button" onClick={pause} />
+         </>
+      )
+   }
+
+   function Paused(props) {
+      return (
+         <>
+            <Icon name='play circle' size="massive" className="icon-button" onClick={play} />
+         </>
+      )
    }
 
    return (
-      <div>
+      <Container className="p-2 center">
          {/* {authorized ? <div><Timer currentTime={currentTime} onTick={onTick} counting={playing} onFinished={onFinished} /></div> : null} */}
-         {authorized ? <Player playing={playing}/> : <Authorize link={authenticateLink} />}
-      </div>
+         {/* {authorized ? <Player playing={playing}/> : <Authorize link={authenticateLink} />} */}
+
+
+         {authorized &&
+            <div className="d-flex">
+               {/* <ReactCountdownClock paused={!playing} seconds={60} color="#000" size={300} onComplete={onFinished} /> */}
+               <Timer renderType="seconds" currentTime={currentTime} onTick={onTick} counting={playing} onFinished={onFinished} />
+            </div>
+         }
+         {authorized ? <Player showControl={!playing} playing={playing} /> : <Authorize link={authenticateLink} />}
+         {/* </Segment> */}
+      </Container>
    );
 }
--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 export interface TimerProps {
    currentTime: number;
    counting: boolean;
+   renderType: 'seconds' | 'minutes';
    onFinished: () => void;
    onTick: () => void;
 }
@@ -18,41 +19,56 @@ export default class Timer extends React.PureComponent<TimerProps, any> {
       super(props);
    }
 
-   componentDidMount(){
-      if(this.props.counting){
+   componentDidMount() {
+      if (this.props.counting) {
          this.start();
       }
    }
 
-   componentDidUpdate(prevProps){
-      if(this.props.counting !== prevProps.counting){
-         if(this.props.counting){
+   componentDidUpdate(prevProps) {
+      if (this.props.counting !== prevProps.counting) {
+         if (this.props.counting) {
             this.start();
-         }else{
+         } else {
             clearInterval(this.interval);
          }
       }
    }
 
-   private start(){
+   componentWillUnmount(){
+      clearInterval(this.interval);
+   }
+
+   private start() {
       this.interval = setInterval(this.tick, 1000);
    }
 
    private tick = () => {
-      if(this.props.currentTime - 1 == 0){
+      if (this.props.currentTime - 1 == 0) {
          //clearInterval(this.interval);
-         this.props.onFinished();
+         //this.props.onFinished();
       }
 
       this.props.onTick();
    }
 
-   private renderTime(){
-      const minutes = Math.floor(this.props.currentTime / 60);
-      const seconds = ('0' + this.props.currentTime % 60).substr(-2, 2);
+   private renderTime() {
 
-      return `${seconds}`;
+      if (this.props.renderType === 'minutes') {
+         const minutes = Math.floor(this.props.currentTime / 60);
+         const seconds = ('0' + this.props.currentTime % 60).substr(-2, 2);
+
+         return `${minutes}:${seconds}`;
+      }
+
+      if (this.props.renderType === 'seconds') {
+         const seconds = ('0' + this.props.currentTime).substr(-2, 2);
+
+         return `${seconds}`;
+      }
    }
+
+
 
    public render() {
       return (
